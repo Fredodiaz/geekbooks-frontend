303fcc2d91fcc710cc7107258baf14622bf3675b
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-18 07:02:43 -0700

    feat(App.js): Added navigation

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
+// Native Imports
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+// Navigation
+import GeekBooksNavigation from './src/components/navigation/GeekBooksNavigation'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <GeekBooksNavigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
