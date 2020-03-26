79519bb9896269fcc1cd36efb16da3033d7e6de5
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-26 10:29:50 -0700

    feat(actions): Added types

---
+++ src/actions/types.js
@@ -0,0 +1,13 @@
+// AUTHORIZATION
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// FORM
+export const HAS_ACCOUNT = 'HAS_ACCOUNT';
+export const IS_FORM_OPEN = 'IS_FORM_OPEN';
