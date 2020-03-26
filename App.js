ab070160d6dc4392023700f8a4d6a38c8812b0de
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-26 10:31:28 -0700

    feat(App.js): Added Store

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import React from 'react';
 // Navigation
 import GeekBooksNavigation from './src/components/navigation/GeekBooksNavigation'
 
+// Store
+import { Provider } from 'react-redux'
+import store from './src/store'
+
 export default function App() {
   return (
-    <GeekBooksNavigation />
+    <Provider store={store}>
+      <GeekBooksNavigation />
+    </Provider>
   );
 }
