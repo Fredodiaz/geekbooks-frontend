fb07c50efb16e003b8836b0465265cc6eb153c41
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-26 10:36:49 -0700

    feat(HomeScreen): Added login/register modal

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,12 +1,38 @@
 // Native Imports
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Image, Modal, StyleSheet } from 'react-native'
 
-const HomeScreen = ({navigation}) => {
+// Redux
+import { connect } from 'react-redux'
+
+// Actions
+import { hasAccount, toggleForm } from '../../actions/formActions'
+
+// Components
+import LoginForm from '../loginForm'
+import RegisterForm from '../registerForm'
+import Button from '../Button'
+
+const HomeScreen = (props) => {
+    const {navigation, form, toggleForm } = props
+    const uri = 'https://bitwiseindustries.com/wp-content/uploads/2020/03/owl.png'
+
     return (
         <View style={styles.container}>
-            <Text>This is the Home Screen!</Text>
-            <Button onPress={() => navigation.navigate({routeName: 'Dashboard'})} title="Go to Dashboard"/>
+
+            <Image style={styles.image} source={{uri}}/>
+            <Text style={styles.header}>GeekBooks</Text>
+            <Button onSelect={() => toggleForm()}style={styles.button} title="Login / Register" 
+                width={'60%'} marginB={20}/>
+            <Button onSelect={() => navigation.navigate({routeName: 'Dashboard'})} title="Get Started" 
+                width={'60%'}/>
+
+            { form.isFormOpen ? 
+                <Modal  animationType="slide">
+                    { form.hasAccount ? <LoginForm /> : <RegisterForm /> }
+                </Modal> 
+            : null}
+
         </View>
     )
 }
@@ -16,7 +42,18 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+    header: {
+        fontSize: 50,
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
 })
 
-export default HomeScreen
+const mapStateToProps = (state) => ({
+    form: state.form
+})
+
+export default connect(mapStateToProps, { toggleForm, hasAccount })(HomeScreen)
