f9f17d8edc15942f7972d033d2336c2b544f6307
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-04-27 16:12:54 -0700

    feat(actions): added action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,13 +1,24 @@
 // AUTHORIZATION
-export const USER_LOADING = 'USER_LOADING';
-export const USER_LOADED = 'USER_LOADED';
-export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REDO_REGISTER = 'REDO_REGISTER'
 
 // FORM
 export const HAS_ACCOUNT = 'HAS_ACCOUNT';
 export const IS_FORM_OPEN = 'IS_FORM_OPEN';
+
+// URL
+export const URL = 'https://geekbooks-api.herokuapp.com'
+
+
+
+// Experimental AUTH
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const LOGOUT_FAIL = 'LOGOUT_FAIL';
