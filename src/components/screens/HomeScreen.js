a29471c7f86074a38d579225892be6bc400e6412
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-04-27 16:22:48 -0700

    feat(HomeScreen): owl + form popup

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -7,6 +7,7 @@ import { connect } from 'react-redux'
 
 // Actions
 import { hasAccount, toggleForm } from '../../actions/formActions'
+import { logout } from '../../actions/authActions'
 
 // Components
 import LoginForm from '../loginForm'
@@ -14,25 +15,34 @@ import RegisterForm from '../registerForm'
 import Button from '../Button'
 
 const HomeScreen = (props) => {
-    const {navigation, form, toggleForm } = props
+    const {navigation, form, toggleForm, auth, logout } = props
     const uri = 'https://bitwiseindustries.com/wp-content/uploads/2020/03/owl.png'
+    
+    let msg = 'Login / Register'
+    auth.isRegistered ? msg = 'Login' : msg = 'Login / Register'
 
     return (
         <View style={styles.container}>
 
             <Image style={styles.image} source={{uri}}/>
             <Text style={styles.header}>GeekBooks</Text>
-            <Button onSelect={() => toggleForm()}style={styles.button} title="Login / Register" 
+
+            { auth.isAuthenticated ? 
+            <Button onSelect={() => logout()}style={styles.button} title="Logout" 
+            width={'60%'} marginB={20}/> :
+            <Button onSelect={() => toggleForm()}style={styles.button} title={msg} 
                 width={'60%'} marginB={20}/>
+            }
+
             <Button onSelect={() => navigation.navigate({routeName: 'Dashboard'})} title="Get Started" 
                 width={'60%'}/>
 
-            { form.isFormOpen ? 
+            { form.isFormOpen && !auth.isAuthenticated ? 
                 <Modal  animationType="slide">
                     { form.hasAccount ? <LoginForm /> : <RegisterForm /> }
                 </Modal> 
             : null}
-
+            {/* <Text style={{fontSize: 30}} onPress={() => console.log(auth)}>Test Me</Text> */}
         </View>
     )
 }
@@ -44,7 +54,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     header: {
-        fontSize: 50,
+        fontSize: 40,
+        marginTop: 0,
+        marginBottom: 15,
     },
     image: {
         width: 200,
@@ -53,7 +65,8 @@ const styles = StyleSheet.create({
 })
 
 const mapStateToProps = (state) => ({
-    form: state.form
+    form: state.form,
+    auth: state.auth
 })
 
-export default connect(mapStateToProps, { toggleForm, hasAccount })(HomeScreen)
+export default connect(mapStateToProps, { toggleForm, hasAccount, logout })(HomeScreen)
